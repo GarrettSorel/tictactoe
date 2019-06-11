2cf3f93a3aa02c2bc0ca2b5809104e6150c0c8b6
Garrett Sorel <garrettsorel@gmail.com>
2019-06-11 14:31:05 -0400

    Update tictactoe.rb

--- tictactoe.rb
+++ tictactoe.rb
@@ -36,47 +36,47 @@ class Game
           @@cells[0] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 2
           @@cells[1] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 3
           @@cells[2] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 4
           @@cells[3] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 5
           @@cells[4] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 6
           @@cells[5] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 7
           @@cells[6] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 8
           @@cells[7] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         when 9
           @@cells[8] = @current_player
           @board.update
           turn
-          select
+          select unless victory?
         else
           puts "Invalid move. Please enter an available number."
           select
@@ -87,6 +87,31 @@ class Game
     end
   end
 
+  def victory?
+    if (@@cells[0] == "X" && @@cells[1] == "X" && @@cells[2] == "X") ||
+       (@@cells[2] == "X" && @@cells[3] == "X" && @@cells[4] == "X") ||
+       (@@cells[6] == "X" && @@cells[7] == "X" && @@cells[8] == "X") ||
+       (@@cells[0] == "X" && @@cells[3] == "X" && @@cells[6] == "X") ||
+       (@@cells[1] == "X" && @@cells[4] == "X" && @@cells[7] == "X") ||
+       (@@cells[2] == "X" && @@cells[5] == "X" && @@cells[8] == "X") ||
+       (@@cells[0] == "X" && @@cells[4] == "X" && @@cells[8] == "X") ||
+       (@@cells[2] == "X" && @@cells[4] == "X" && @@cells[6] == "X")
+         puts "X Wins!"
+         @board.reset
+    elsif (@@cells[0] == "O" && @@cells[1] == "O" && @@cells[2] == "O") ||
+       (@@cells[2] == "O" && @@cells[3] == "O" && @@cells[4] == "O") ||
+       (@@cells[6] == "O" && @@cells[7] == "O" && @@cells[8] == "O") ||
+       (@@cells[0] == "O" && @@cells[3] == "O" && @@cells[6] == "O") ||
+       (@@cells[1] == "O" && @@cells[4] == "O" && @@cells[7] == "O") ||
+       (@@cells[2] == "O" && @@cells[5] == "O" && @@cells[8] == "O") ||
+       (@@cells[0] == "O" && @@cells[4] == "O" && @@cells[8] == "O") ||
+       (@@cells[2] == "O" && @@cells[4] == "O" && @@cells[6] == "O")
+         puts "O Wins!"
+         @board.reset    
+    else
+      select
+    end
+  end
 end
 
 class GameBoard < Game
@@ -98,6 +123,11 @@ class GameBoard < Game
     puts "#{@@cells[0]} | #{@@cells[1]} | #{@@cells[2]}"
     puts "#{@@cells[3]} | #{@@cells[4]} | #{@@cells[5]}"
     puts "#{@@cells[6]} | #{@@cells[7]} | #{@@cells[8]}"
+  end
+
+  def reset
+    @@cells = [1,2,3,4,5,6,7,8,9]
+    update
   end
 
 end
